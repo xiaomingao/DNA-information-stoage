a2a7daa904f94bc11a821d4441b9e5f87a230293
xiaomingao <56572982+xiaomingao@users.noreply.github.com>
2019-11-11 17:09:43 +0800

    Add files via upload

---
+++ Windows/decode/decode_random.pl
@@ -0,0 +1,28 @@
+#perl decode_random.pl ./temp 10751 3
+use List::Util 'shuffle';
+use File::Copy;
+#$input = "./temp";
+$input = $ARGV[0];
+$num = $ARGV[1];
+$times = $ARGV[2];
+#$output = "./temp2/decode_file_cod";
+
+opendir(DIR, $input);
+@file = readdir(DIR);
+print @file."\n";
+for($i=0;$i<$times;$i++)
+{
+	
+	$folder = "./temp".$i;	
+	mkdir($folder);
+	copy("./encode/encode_file_meta.txt", $folder);
+	@array1 = shuffle(@file);
+	for($m=0;$m<$num;$m++)
+	{
+		$one = $input."/".$array1[$m];
+		$other = $folder."/decode_file_cod".$m;
+		copy($one,$other);
+	}
+}
+
+#meta file
---
+++ Windows/decode/repeat_remove_binary_2.pl
@@ -0,0 +1,32 @@
+#perl ./lib/repeat_remove_binary_2.pl ./temp
+use File::Compare;
+#$input = "./temp";
+$output = $ARGV[1];
+open CA,$input;
+opendir(DIR, $input);
+@file = readdir(DIR);
+$i=$m=0;
+foreach $file (@file)
+{
+	$new = $input."/".$file;
+	open CA,$new;
+	$seq="";
+	while($line1 = <CA>)
+	{
+		$seq=$seq.$line1;
+	}
+	close CA;
+	if(not defined($list{$seq}))
+	{
+		$list{$seq}=$new;
+#		$new_file = $output.$i;
+		$i++;
+		#print $new."\n";
+	}
+	else {
+		unlink($new);
+		#print $new."\t".$list{$seq}."\n";
+	}
+}
+print "Total decode set\t".$i."\n";
+#meta file
